c77987352d6aee384c6f13f65a78190190238fee
Daniel Olojakpoke <danielolojakpoke@gmail.com>
2026-09-02 20:22:36 +0100

    fix: trigger deployment

--- src/winston.ts
+++ src/winston.ts
@@ -2,6 +2,7 @@ import winston from "winston";
 import config from "./infra/get-config.js";
 
 const logLevel = config.LOG_LEVEL ?? "info";
+
 console.log(`log level set to ${logLevel}`);
 
 const logger = winston.createLogger({
